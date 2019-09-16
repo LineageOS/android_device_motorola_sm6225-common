654435f4adf54485a3d77881f924d3c21bec82bf
Jiyong Park <jiyong@google.com>
2019-09-16 10:34:32 +0200

    sm6225-common: Add updatable_apex.mk
    The product abstracts configs that need to be set to support updating of
    APEXes.
    
    Change-Id: Id5aa944ceba6a6846308f1c18a08af8b3bb38589

--- bengal.mk
+++ bengal.mk
@@ -7,6 +7,9 @@
 # Include GSI keys
 $(call inherit-product, $(SRC_TARGET_DIR)/product/gsi_keys.mk)
 
+# Enable updating of APEXes
+$(call inherit-product, $(SRC_TARGET_DIR)/product/updatable_apex.mk)
+
 # A/B
 $(call inherit-product, $(SRC_TARGET_DIR)/product/virtual_ab_ota/launch_with_vendor_ramdisk.mk)
 
