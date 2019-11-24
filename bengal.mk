988ef7c445c5860310cbbb173d9a57fe9c713fb8
LuK1337 <priv.luk@gmail.com>
2019-11-24 20:25:39 +0100

    sm6225-common: overlay: Add reboot to fastbootd
    Change-Id: I733b4d29129f85b92222e292af46602310b9f29f

--- bengal.mk
+++ bengal.mk
@@ -280,7 +280,8 @@ PRODUCT_PACKAGES += \
 
 # Overlay
 DEVICE_PACKAGE_OVERLAYS += \
-    $(LOCAL_PATH)/overlay
+    $(LOCAL_PATH)/overlay \
+    $(LOCAL_PATH)/overlay-lineage
 
 PRODUCT_ENFORCE_RRO_TARGETS := *
 
