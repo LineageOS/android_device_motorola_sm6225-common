720e8838d261538f4cea2cb8707afb873981cab7
Bruno Martins <bgcngm@gmail.com>
2022-04-05 10:34:38 +0100

    sm6225-common: Inherit from QTI FCM
    Change-Id: I0bdd4d63b60a1e29cdd69e71f7268932a105c16b

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -43,7 +43,9 @@ TARGET_FS_CONFIG_GEN := \
     $(COMMON_PATH)/mot_aids.fs
 
 # HIDL
-DEVICE_FRAMEWORK_COMPATIBILITY_MATRIX_FILE := $(COMMON_PATH)/framework_compatibility_matrix.xml
+DEVICE_FRAMEWORK_COMPATIBILITY_MATRIX_FILE := \
+    $(COMMON_PATH)/framework_compatibility_matrix.xml \
+    hardware/qcom-caf/common/vendor_framework_compatibility_matrix.xml
 DEVICE_MANIFEST_FILE := $(COMMON_PATH)/manifest.xml
 DEVICE_MATRIX_FILE := $(COMMON_PATH)/compatibility_matrix.xml
 
