c51f172bec42367acbfa6a6ef9bce09ca384445d
electimon <electimon@gmail.com>
2022-04-21 08:11:25 +0000

    sm6225-common: Rename camera.mot.is.coming.cts
    * This prop is illegal due to violating the
    * vendor property namespace rules
    
    [Dhina17: Adapted for sm6225]
    
    Signed-off-by: electimon <electimon@gmail.com>
    Change-Id: I2da4951e19e97e5291cbb0f0b18cdae14f63fbaa

--- extract-files.sh
+++ extract-files.sh
@@ -63,6 +63,21 @@ function blob_fixup() {
         system_ext/etc/permissions/moto-telephony.xml)
             sed -i "s#/system/#/system_ext/#" "${2}"
             ;;
+        vendor/lib64/camera/components/com.qti.node.gpu.so)
+            sed -i "s/camera.mot.is.coming.cts/vendor.camera.coming.cts/g" "${2}"
+            ;;
+        vendor/lib64/com.qti.feature2.gs.so)
+            sed -i "s/camera.mot.is.coming.cts/vendor.camera.coming.cts/g" "${2}"
+            ;;
+        vendor/lib64/com.qti.feature2.rt.so)
+            sed -i "s/camera.mot.is.coming.cts/vendor.camera.coming.cts/g" "${2}"
+            ;;
+        vendor/lib64/hw/camera.qcom.so)
+            sed -i "s/camera.mot.is.coming.cts/vendor.camera.coming.cts/g" "${2}"
+            ;;
+        vendor/lib64/hw/com.qti.chi.override.so)
+            sed -i "s/camera.mot.is.coming.cts/vendor.camera.coming.cts/g" "${2}"
+            ;;
         vendor/lib64/vendor.qti.hardware.camera.postproc@1.0-service-impl.so)
             hexdump -ve '1/1 "%.2X"' "${2}" | sed "s/130A0094/1F2003D5/g" | xxd -r -p > "${EXTRACT_TMP_DIR}/${1##*/}"
             mv "${EXTRACT_TMP_DIR}/${1##*/}" "${2}"
