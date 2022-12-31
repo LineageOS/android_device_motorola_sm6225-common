8e445426a06bc65ede49a2de361603f11010dd7a
Dhina17 <dhinalogu@gmail.com>
2022-12-31 10:54:17 +0530

    sm6225-common: Define arch
    - Taken from CodeLinaro
    
    Change-Id: I905059d33b9bd55db5d79e3930e9593d579de39a

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -6,6 +6,19 @@
 
 COMMON_PATH := device/motorola/sm6225-common
 
+# Architecture
+TARGET_ARCH := arm64
+TARGET_ARCH_VARIANT := armv8-a
+TARGET_CPU_ABI := arm64-v8a
+TARGET_CPU_ABI2 :=
+TARGET_CPU_VARIANT := generic
+
+TARGET_2ND_ARCH := arm
+TARGET_2ND_ARCH_VARIANT := armv7-a-neon
+TARGET_2ND_CPU_ABI := armeabi-v7a
+TARGET_2ND_CPU_ABI2 := armeabi
+TARGET_2ND_CPU_VARIANT := cortex-a9
+
 # Platform
 BOARD_USES_QCOM_HARDWARE := true
 TARGET_BOARD_PLATFORM := bengal
