bbeabec192956b6f20dec9086b0712d50acec53d
Dhina17 <dhinalogu@gmail.com>
2023-02-11 13:45:12 +0530

    sm6225-common: Import bdroid_buildcfg.h from CodeLinaro
    TAG: LA.UM.9.15.r1-05800-KAMORTA.QSSI12.0
    
    Change-Id: Iec7f74b528b8cf4534c877632b0ac958a9785d8a

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -19,6 +19,9 @@ TARGET_2ND_CPU_ABI := armeabi-v7a
 TARGET_2ND_CPU_ABI2 := armeabi
 TARGET_2ND_CPU_VARIANT := cortex-a9
 
+# Bluetooth
+BOARD_BLUETOOTH_BDROID_BUILDCFG_INCLUDE_DIR := $(COMMON_PATH)/bluetooth/include
+
 # Bootloader
 TARGET_BOOTLOADER_BOARD_NAME := bengal
 TARGET_NO_BOOTLOADER := true
