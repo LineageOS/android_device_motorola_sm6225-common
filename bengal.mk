4af16ddc47ae9a1c4e5944bd6748c453303f0537
Dhina17 <dhinalogu@gmail.com>
2023-02-11 18:55:33 +0530

    sm6225-common: Build media packages
    Change-Id: Ic2d1627224bc4577034860df9f6ff515c3b6dcbd

--- bengal.mk
+++ bengal.mk
@@ -177,6 +177,18 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     android.hardware.keymaster@4.1.vendor
 
+# Media
+PRODUCT_PACKAGES += \
+    libmm-omxcore \
+    libOmxCore \
+    libOmxVdec \
+    libOmxVenc \
+    libstagefrighthw \
+    libc2dcolorconvert
+
+PRODUCT_PACKAGES += \
+    libavservices_minijail.vendor
+
 # Media - Configs
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/media/media_codecs.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_codecs.xml \
