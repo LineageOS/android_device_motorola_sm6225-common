61a39b9a2b84c945c3d087b76b44643344db04f4
Dhina17 <dhinalogu@gmail.com>
2023-02-12 01:23:45 +0530

    sm6225-common: Set board and product shipping api level
    - bengal is freezed at 30. (GRF)
    - All products (devices - hawao, devon, rhode) launched with api level 31.
    
    Change-Id: Ia6e3843d4dc07a515ee65573b75141f87b5613b7

--- bengal.mk
+++ bengal.mk
@@ -4,6 +4,10 @@
 # SPDX-License-Identifier: Apache-2.0
 #
 
+# Shipping API level
+BOARD_API_LEVEL := 30
+BOARD_SHIPPING_API_LEVEL := 30
+PRODUCT_SHIPPING_API_LEVEL := 31
 
 # Soong namespaces
 PRODUCT_SOONG_NAMESPACES += \
