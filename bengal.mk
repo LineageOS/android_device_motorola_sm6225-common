0072d26deda61ece5ba39b26df3a3ab7282ba763
Dhina17 <dhinalogu@gmail.com>
2023-02-11 16:30:37 +0530

    sm6225-common: Build camera packages
    Change-Id: I471162d5cd1fcec1bcbd914532bd9fc42745d859

--- bengal.mk
+++ bengal.mk
@@ -94,6 +94,19 @@ PRODUCT_PACKAGES += \
     android.hardware.boot@1.1-impl-qti.recovery \
     android.hardware.boot@1.1-service
 
+# Camera
+PRODUCT_PACKAGES += \
+    android.hardware.camera.provider@2.4-impl \
+    android.hardware.camera.provider@2.4-service_64
+
+PRODUCT_PACKAGES += \
+    android.frameworks.sensorservice@1.0 \
+    android.frameworks.sensorservice@1.0.vendor \
+    vendor.qti.hardware.camera.postproc@1.0.vendor
+
+PRODUCT_PACKAGES += \
+    libgui_vendor
+
 # Filesystem
 PRODUCT_PACKAGES += \
     fs_config_files
