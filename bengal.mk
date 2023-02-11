cae638f1122b32939f0a85167991658e74fc15db
Dhina17 <dhinalogu@gmail.com>
2023-02-11 19:28:04 +0530

    sm6225-common: Build QTI USB 1.3
    Change-Id: Ic5be4a49c8b31099820b46a95026221f411b45de

--- bengal.mk
+++ bengal.mk
@@ -326,7 +326,8 @@ PRODUCT_SHIPPING_API_LEVEL := 31
 
 # Soong namespaces
 PRODUCT_SOONG_NAMESPACES += \
-    $(LOCAL_PATH)
+    $(LOCAL_PATH) \
+    vendor/qcom/opensource/usb/etc
 
 # Telephony
 PRODUCT_PACKAGES += \
@@ -342,6 +343,14 @@ PRODUCT_PACKAGES += \
 
 PRODUCT_BOOT_JARS += \
     telephony-ext
+
+# USB
+PRODUCT_PACKAGES += \
+    android.hardware.usb@1.3-service-qti
+
+PRODUCT_PACKAGES += \
+    init.qcom.usb.rc \
+    init.qcom.usb.sh
 
 # Vendor ramdisk
 PRODUCT_COPY_FILES += \
