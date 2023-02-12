3e26abb480fb544c09a82409b5b7d860076cdcff
Dhina17 <dhinalogu@gmail.com>
2023-02-12 09:40:51 +0530

    sm6225-common: Build libhidltransport and libhwbinder
    - Needed by fingerprint and soter HIDL.
    
    Change-Id: Ie5f74e6f90009e8011e2fa40cab90e1cbf87fbca

--- bengal.mk
+++ bengal.mk
@@ -173,6 +173,11 @@ PRODUCT_PACKAGES += \
     android.hardware.health@2.1-impl.recovery \
     android.hardware.health@2.1-service
 
+# HIDL
+PRODUCT_PACKAGES += \
+    libhidltransport.vendor \
+    libhwbinder.vendor
+
 # IPACM
 PRODUCT_PACKAGES += \
     ipacm \
