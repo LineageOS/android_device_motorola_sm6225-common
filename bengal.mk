8f7b314ab6ec7007ddc85c44b6ea682fe9224e7b
Dhina17 <dhinalogu@gmail.com>
2023-02-12 09:27:02 +0530

    sm6225-common: Build netd 1.1 lib
    - needed by netmgrd.
    
    Change-Id: I5f1e7fa57d924fe1bb6989a3a8d02871d14df70a

--- bengal.mk
+++ bengal.mk
@@ -222,6 +222,10 @@ PRODUCT_COPY_FILES += \
     frameworks/av/media/libstagefright/data/media_codecs_google_video.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_codecs_google_video.xml \
     frameworks/av/media/libstagefright/data/media_codecs_google_video_le.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_codecs_google_video_le.xml
 
+# Net
+PRODUCT_PACKAGES += \
+    android.system.net.netd@1.1.vendor
+
 # Neural networks
 PRODUCT_PACKAGES += \
     android.hardware.neuralnetworks@1.3.vendor
