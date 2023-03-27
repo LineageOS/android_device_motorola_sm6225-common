39179db1901005741d546a66cb94600d6784d9a2
Gabriel Biren <gbiren@google.com>
2023-03-27 22:33:41 +0000

    sm6225-common: Update makefile to use the AIDL Wifi Vendor HAL.
    Bug: 274964641
    Test: Pre-submit tests
    Change-Id: I4d9114377fe75b593bbfe6a59f03d8f5d607d6a1

--- bengal.mk
+++ bengal.mk
@@ -451,7 +451,7 @@ PRODUCT_COPY_FILES += \
 
 # Wifi
 PRODUCT_PACKAGES += \
-    android.hardware.wifi@1.0-service \
+    android.hardware.wifi-service \
     hostapd \
     libqsap_sdk \
     libwpa_client \
