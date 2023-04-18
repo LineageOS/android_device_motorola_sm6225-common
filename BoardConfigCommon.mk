af60676a5af8e1fd0cd5adfe3dd9bfb11aaf0526
Michael Bestas <mkbestas@lineageos.org>
2023-04-18 23:27:15 +0300

    sm6225-common: Add com.dsi.ant to vintf manifest
    Fixes:
    E HidlServiceManagement: Service com.dsi.ant@1.0::IAnt/default must be in VINTF manifest in order to register/get.
    E LegacySupport: Could not register service com.dsi.ant@1.0::IAnt/default (-2147483648).
    I android.hardware.bluetooth@1.0-service: Error while registering ANT service: -2147483648
    
    Change-Id: Ifec99544b90dcf11f83e22c0436b89bb1a279e6a

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -55,6 +55,7 @@ DEVICE_FRAMEWORK_COMPATIBILITY_MATRIX_FILE := \
     hardware/qcom-caf/common/vendor_framework_compatibility_matrix.xml
 DEVICE_MANIFEST_FILE := $(COMMON_PATH)/manifest.xml
 DEVICE_MATRIX_FILE := $(COMMON_PATH)/compatibility_matrix.xml
+ODM_MANIFEST_FILES := $(COMMON_PATH)/manifest-qva.xml
 
 # Kernel
 BOARD_BOOT_HEADER_VERSION := 3
