2ac59ca966a094831018040e7bfd043487d80f59
Michael Bestas <mkbestas@lineageos.org>
2023-04-21 17:28:44 +0300

    sm6225-common: Build com.motorola.hardware.biometric.fingerprint@1.0 from source
    Change-Id: I9bfb3cdb86097e2df8bd0d4e5446c7dd84622a77

--- bengal.mk
+++ bengal.mk
@@ -186,7 +186,8 @@ PRODUCT_PACKAGES += \
 
 # Fingerprint
 PRODUCT_PACKAGES += \
-    android.hardware.biometrics.fingerprint@2.1.vendor
+    android.hardware.biometrics.fingerprint@2.1.vendor \
+    com.motorola.hardware.biometric.fingerprint@1.0.vendor
 
 # Framework detect
 PRODUCT_PACKAGES += \
