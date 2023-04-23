f0fa6fd7aa82366382bfe55ca8e6affbb8e4b2a9
Michael Bestas <mkbestas@lineageos.org>
2023-04-23 21:00:08 +0300

    sm6225-common: Remove unused audio_ext_spkr.conf
    Change-Id: I0184d5a15cf3c343fd032d518ace2b5ac4117d10

--- bengal.mk
+++ bengal.mk
@@ -92,7 +92,6 @@ PRODUCT_PACKAGES += \
 # Audio - Configs
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/audio/audio_effects.xml:$(TARGET_COPY_OUT_VENDOR)/etc/audio_effects.xml \
-    $(LOCAL_PATH)/audio/audio_ext_spkr.conf:$(TARGET_COPY_OUT_VENDOR)/etc/audio_ext_spkr.conf \
     $(LOCAL_PATH)/audio/audio_policy_configuration.xml:$(TARGET_COPY_OUT_VENDOR)/etc/audio_policy_configuration.xml \
     $(LOCAL_PATH)/audio/audio_policy_configuration_a2dp_offload_disabled.xml:$(TARGET_COPY_OUT_VENDOR)/etc/audio_policy_configuration_a2dp_offload_disabled.xml \
     $(LOCAL_PATH)/audio/audio_policy_configuration_vendor.xml:$(TARGET_COPY_OUT_VENDOR)/etc/audio/audio_policy_configuration.xml \
