caebeb5c109cd1e9809af14f4a269c19d8b85b10
Michael Bestas <mkbestas@lineageos.org>
2023-04-23 21:00:40 +0300

    sm6225-common: Import audio_io_policy.conf from CLO
    Change-Id: Ia14eba4ceda9294a1f38800443c8ca333a380b71

--- bengal.mk
+++ bengal.mk
@@ -92,6 +92,7 @@ PRODUCT_PACKAGES += \
 # Audio - Configs
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/audio/audio_effects.xml:$(TARGET_COPY_OUT_VENDOR)/etc/audio_effects.xml \
+    $(LOCAL_PATH)/audio/audio_io_policy.conf:$(TARGET_COPY_OUT_VENDOR)/etc/audio_io_policy.conf \
     $(LOCAL_PATH)/audio/audio_policy_configuration.xml:$(TARGET_COPY_OUT_VENDOR)/etc/audio_policy_configuration.xml \
     $(LOCAL_PATH)/audio/audio_policy_configuration_a2dp_offload_disabled.xml:$(TARGET_COPY_OUT_VENDOR)/etc/audio_policy_configuration_a2dp_offload_disabled.xml \
     $(LOCAL_PATH)/audio/audio_policy_configuration_vendor.xml:$(TARGET_COPY_OUT_VENDOR)/etc/audio/audio_policy_configuration.xml \
