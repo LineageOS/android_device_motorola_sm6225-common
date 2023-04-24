22440349fa7c19b3a4d6f50fbc95143ca619aa93
Michael Bestas <mkbestas@lineageos.org>
2023-04-24 19:52:25 +0300

    sm6225-common: Add devon & rhode to build guards
    Change-Id: If5c3292acc54512d211c7f966c9c8d0d17c3c87f

--- Android.mk
+++ Android.mk
@@ -6,7 +6,7 @@
 
 LOCAL_PATH := $(call my-dir)
 
-ifneq ($(filter hawao, $(TARGET_DEVICE)),)
+ifneq ($(filter devon hawao rhode, $(TARGET_DEVICE)),)
 include $(call all-makefiles-under, $(LOCAL_PATH))
 
 include $(CLEAR_VARS)
--- setup-makefiles.sh
+++ setup-makefiles.sh
@@ -25,7 +25,7 @@ source "${HELPER}"
 setup_vendor "${DEVICE_COMMON}" "${VENDOR}" "${ANDROID_ROOT}" true
 
 # Warning headers and guards
-write_headers "hawao"
+write_headers "devon hawao rhode"
 
 # The standard common blobs
 write_makefiles "${MY_DIR}/proprietary-files.txt" true
