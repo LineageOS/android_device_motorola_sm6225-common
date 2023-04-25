23512322930ce5d6f634ec5b58f3247b97aa3d3d
Michael Bestas <mkbestas@lineageos.org>
2023-04-25 17:18:24 +0300

    sm6225-common: Decommonize security patch level
    Change-Id: I2e418eb19051406a6631c97d2029606caeb5f3a5

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -182,9 +182,6 @@ BOARD_USES_RECOVERY_AS_BOOT := true
 TARGET_RECOVERY_FSTAB := $(COMMON_PATH)/rootdir/etc/fstab.qcom
 TARGET_RECOVERY_PIXEL_FORMAT := "RGBX_8888"
 
-# Security patch level
-VENDOR_SECURITY_PATCH := 2023-02-01
-
 # Sepolicy
 include device/qcom/sepolicy_vndr-legacy-um/SEPolicy.mk
 BOARD_VENDOR_SEPOLICY_DIRS += $(COMMON_PATH)/sepolicy/vendor
