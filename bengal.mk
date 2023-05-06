f53140d81745cf6c7a5095f6c9c8a65a652a48c3
Michael Bestas <mkbestas@lineageos.org>
2023-05-06 17:54:38 +0300

    sm6225-common: Decommonize boot animation size
    Change-Id: I59f694d2abea5d225317605cc819115f16f1fef9

--- bengal.mk
+++ bengal.mk
@@ -118,10 +118,6 @@ PRODUCT_PACKAGES += \
     vendor.qti.hardware.btconfigstore@1.0.vendor \
     vendor.qti.hardware.btconfigstore@2.0.vendor
 
-# Boot animation
-TARGET_SCREEN_HEIGHT := 2400
-TARGET_SCREEN_WIDTH := 1080
-
 # Bootctrl
 PRODUCT_PACKAGES += \
     android.hardware.boot@1.1-impl-qti \
