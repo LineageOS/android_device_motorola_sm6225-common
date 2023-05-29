75b00aebb2608db42a1458cc8555f09e30797d80
Nolen Johnson <johnsonnolen@gmail.com>
2023-05-29 15:46:59 -0400

    sm6225-common: Build MotoActions
    * We already track hw/moto.
    
    Change-Id: I9e935b31cda95b70c3398f798fd37b64b1984ac1

--- bengal.mk
+++ bengal.mk
@@ -279,6 +279,10 @@ PRODUCT_COPY_FILES += \
     frameworks/av/media/libstagefright/data/media_codecs_google_video.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_codecs_google_video.xml \
     frameworks/av/media/libstagefright/data/media_codecs_google_video_le.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_codecs_google_video_le.xml
 
+# MotoActions
+PRODUCT_PACKAGES += \
+    MotoActions
+
 # Net
 PRODUCT_PACKAGES += \
     android.system.net.netd@1.1.vendor
