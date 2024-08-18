186dbb8720edaebd06aba58024cef7cad91a4f4b
Cosmin Tanislav <demonsingur@gmail.com>
2024-08-18 09:52:31 +0300

    sm6225-common: skip dependencies which aren't defined in .bp
    Change-Id: I01a653661856495788c05b720c0577dac44998a4

--- setup-makefiles.sh
+++ setup-makefiles.sh
@@ -43,6 +43,9 @@ function lib_to_package_fixup_vendor_variants() {
             vendor.qti.imsrtpservice@3.0)
             echo "$1-vendor"
             ;;
+        libqsap_sdk | \
+            libril | \
+            libwpa_client) ;;
         *)
             return 1
             ;;
