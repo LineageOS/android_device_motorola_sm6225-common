a100e7bae5de2242de9eb22ea36717c44ec00c05
Michael Bestas <mkbestas@lineageos.org>
2025-03-09 12:45:54 +0200

    sm6225-common: Shim sensors.moto with libbase_shim
    See: https://cs.android.com/android/_/android/platform/system/libbase/+/46fc805282265715266c256fbe34cf1a886c475f
    
    Change-Id: I5127020e18114a13515d9dc382c95d689634243d

--- extract-files.py
+++ extract-files.py
@@ -52,6 +52,8 @@ blob_fixups: blob_fixups_user_type = {
         .sig_replace('13 0A 00 94', '1F 20 03 D5'),
     'vendor/lib64/libwvhidl.so': blob_fixup()
         .add_needed('libcrypto_shim.so'),
+    'vendor/lib64/sensors.moto.so': blob_fixup()
+        .add_needed('libbase_shim.so'),
 }  # fmt: skip
 
 module = ExtractUtilsModule(
