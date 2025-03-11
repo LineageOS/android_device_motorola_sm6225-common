7eaa768514514e7c038e968221dfea97b41c0690
Michael Bestas <mkbestas@lineageos.org>
2025-03-11 02:07:18 +0200

    sm6225-common: Enable ELF checks for libmdmcutback
    Change-Id: I734990bc95adacc1229f649d7e39bf2066abbbe6

--- bengal.mk
+++ bengal.mk
@@ -348,7 +348,6 @@ PRODUCT_COPY_FILES += \
 PRODUCT_PACKAGES += \
     android.hardware.wifi-service \
     hostapd \
-    libqsap_sdk \
     libwifi-hal-qcom \
     wpa_supplicant \
     wpa_supplicant.conf
