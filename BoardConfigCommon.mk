23b37d59eb6a02ae57c3c00f30be87390920a85c
Nolen Johnson <johnsonnolen@gmail.com>
2026-07-01 01:34:49 -0400

    sm6225-common: Use legacy libion implementation
    * At least one blob directly depends on this, with
      many other bits and pieces likely.
    
    Change-Id: Ic063618d6942a4f6e90dd7e21698e36d14113d06

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -158,6 +158,7 @@ BOARD_INCLUDE_RECOVERY_DTBO := true
 TARGET_RECOVERY_PIXEL_FORMAT := RGBX_8888
 
 # Sepolicy
+include device/lineage/sepolicy/libion/sepolicy.mk
 include device/qcom/sepolicy_vndr/SEPolicy.mk
 include hardware/motorola/sepolicy/qti/SEPolicy.mk
 
